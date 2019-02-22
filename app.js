84ebd4da268a591eafb86bea2d89aa9216d3bf4e
Carlos <carlos.cadena@correounivalle.edu.co>
2019-02-22 01:01:47 +0000

    Adding bookshelfjs ORM

--- app.js
+++ app.js
@@ -22,13 +22,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contacts', contacts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-app.use('/contacts', contacts);
+
 
 // error handler
 app.use(function(err, req, res, next) {
