c4bcc7cee5f25f737c8f485d80333e665af32dfd
Carlos <carlos.cadena@correounivalle.edu.co>
2019-02-22 00:44:12 +0000

    Adding bookshelfjs ORM

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var contacts = require('./routes/contact');
 
 var app = express();
 
@@ -26,6 +27,8 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+app.use('/contacts', contacts);
 
 // error handler
 app.use(function(err, req, res, next) {
